0b75b769fd6f7774f65ea5a4b18c53b1c2447d71
avin <carcinogen75@gmail.com>
2017-05-09 13:18:27 +0300

    add iptable rule

--- workspace/run.sh
+++ workspace/run.sh
@@ -1,4 +1,5 @@
 echo 1 | tee /proc/sys/net/ipv4/ip_forward
 modprobe iptable_nat
+iptables -t nat -A POSTROUTING -s 10.4.0.1/2 -o eth0 -j MASQUERADE
 cp /etc/openvpn/ovpn.key /data/keys/
 openvpn /etc/openvpn/openvpn.conf
